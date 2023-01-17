71b7e46622f1b7b81c600a07ccace16e3f311045
Dengz <dengweiyi870626@gmail.com>
2023-01-17 18:37:54 +0800

    add ProfileCard

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import ProfileCard from './ProfileCard';
+
+function App() {
+  return (
+    <div>
+      <h1>語音助理清單</h1>
+      <ProfileCard />
+      <ProfileCard />
+      <ProfileCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ProfileCard.js
@@ -0,0 +1,5 @@
+function ProfileCard() {
+  return <h1 className='fold-bold text-4xl text-rose-800'>profileCard</h1>;
+}
+
+export default ProfileCard;
